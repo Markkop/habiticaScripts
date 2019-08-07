fa98295bca34c8022cfb8cdd145a5dce2c7a2f4e
Mark Kop <mk.marcelo.mk@gmail.com>
2019-08-07 09:26:29 -0300

    syncs after updating user stats, visuals

--- customSkills.js
+++ customSkills.js
@@ -19,11 +19,11 @@ const customSkills = [
   {
     name: "Soul Pact",
     imgSrc:
-      "https://gamepedia.cursecdn.com/wizardoflegend_gamepedia_en/thumb/1/11/Raging_Inferno.png/128px-Raging_Inferno.png",
+      "https://www.pngix.com/pngfile/big/95-951782_luciditynexus-pack-magic-circle-png-transparent-png.png",
     multiplier: {
       hp: "-10",
       mp: "-10",
-      exp: "+10",
+      exp: "-10",
       gp: "0"
     }
   },
@@ -65,18 +65,25 @@ const createButtons = (skills, stats) => {
     const divsCosts = Object.keys(skill.multiplier)
       .filter(cost => skill.multiplier[cost] < 0)
       .reduce((str, cost) => {
-        return str.concat(
-          `<div data-v-d5085df8="" class="mana-text" style="padding-top: 0"><div data-v-d5085df8="" class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path fill="#2995CD" d="M22 15l-10 9-10-9L12 0z"></path><path fill="#50B5E9" d="M4.6 14.7l7.4-3v9.6z"></path><path fill="#1F709A" d="M12 11.7l7.4 3-7.4 6.6z" opacity=".25"></path><path fill="#FFF" d="M12 11.7V3.6l7.4 11.1z" opacity=".25"></path><path fill="#FFF" d="M4.6 14.7L12 3.6v8.1z" opacity=".5"></path><path fill="#FFF" d="M7.2 14.3L12 7.2l4.8 7.1-4.8 4.3z" opacity=".5"></path></g></svg></div><div data-v-d5085df8="">${
-            skill.multiplier[cost]
-          }%</div></div>`
+        return str.concat(`
+          <div data-v-d5085df8="" class="mana-text" style="padding-top: 0; display: flex; margin-bottom: 0">
+            ${iconsHtml(cost)}  
+          <div data-v-d5085df8="" style="color: ${costColor(cost)}">${skill.multiplier[cost]}%</div></div>`
         );
       }, "");
 
-    divSpell.innerHTML = `<div data-v-d5085df8="" class="spell col-12 row"><div data-v-d5085df8="" class="col-8 details"><a data-v-d5085df8="" class=""></a><div style="display: inline-block; width: 40px; height: 40px" class="img shop-sprite item-img"><img width="35" src="${
-      skill.imgSrc
-    }" /></div><span data-v-d5085df8="" class="title">${
-      skill.name
-    }</span></div><div data-v-d5085df8="" style="display: flex; flex-direction: column; justify-content: center" class="col-4 mana">${divsCosts}</div></div>`;
+    divSpell.innerHTML = `
+    <div data-v-d5085df8="" class="spell col-12 row">
+      <div data-v-d5085df8="" class="col-8 details">
+        <a data-v-d5085df8="" class=""></a>
+        <div style="display: inline-block; width: 40px; height: 40px" class="img shop-sprite item-img">
+          <img width="35" src="${skill.imgSrc}" />
+        </div>
+        <span data-v-d5085df8="" class="title">${skill.name}</span>
+      </div>
+      <div data-v-d5085df8="" style="display: flex; flex-direction: column; justify-content: center; background-color: #b29fc5; max-height: 45px;" class="col-4 mana">${divsCosts}</div>
+    </div>`;
+
     divSpell.className = "col-12 col-md-3";
 
     const newStats = changedStats(skill, stats);
@@ -106,6 +113,13 @@ const appendSkills = buttons => {
   const spellContainer = document.getElementsByClassName(
     "container spell-container"
   )[0];
+
+  if (!spellContainer) {
+    console.log(
+      `spellContainer is ${spellContainer}, maybe spells weren't loaded yet`
+    );
+  }
+
   spellContainer.appendChild(div);
 };
 
@@ -122,6 +136,7 @@ const putStats = async newStats => {
     body: JSON.stringify(newStats)
   });
   //const data = await resp.json();
+  document.getElementsByClassName("top-menu-icon svg-icon")[5].click();
   await console.log("Put Stats: ", await resp.json());
 };
 
@@ -143,6 +158,36 @@ const main = async () => {
     appendSkills(buttons);
   } catch (err) {
     console.log("Caught:", err);
+  }
+};
+
+const iconsHtml = stat => {
+  switch (stat) {
+    case "hp":
+      return '<div style="width: 16px" data-v-062fc6e8="" class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path fill="#F74E52" d="M2 4.5L6.167 2 12 5.167 17.833 2 22 4.5V12l-4.167 5.833L12 22l-5.833-4.167L2 12z"></path><path fill="#FF6165" d="M7.333 16.667L3.667 11.5V5.417l2.5-1.5L12 7.083l5.833-3.166 2.5 1.5V11.5l-3.666 5.167L12 19.917z"></path><path fill="#FFF" d="M12 14.083l4.667 2.584L12 19.917z" opacity=".5"></path><path fill="#B52428" d="M12 14.083l-4.667 2.584L12 19.917z" opacity=".35"></path><path fill="#FFF" d="M7.333 16.667L3.667 11.5 12 14.083z" opacity=".25"></path><path fill="#B52428" d="M16.667 16.667l3.666-5.167L12 14.083z" opacity=".5"></path><path fill="#B52428" d="M12 14.083l5.833-10.166 2.5 1.5V11.5z" opacity=".35"></path><path fill="#B52428" d="M12 14.083L6.167 3.917l-2.5 1.5V11.5z" opacity=".5"></path><path fill="#FFF" d="M12 14.083L6.167 3.917 12 7.083z" opacity=".5"></path><path fill="#FFF" d="M12 14.083l5.833-10.166L12 7.083z" opacity=".25"></path><path fill="#FFF" d="M9.167 14.833l-3-4.166V6.833h.083L12 9.917l5.75-3.084h.083v3.834l-3 4.166L12 16.917z" opacity=".5"></path></g></svg></div>';
+    case "mp":
+      return '<div style="width: 16px" data-v-062fc6e8="" class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path fill="#2995CD" d="M22 15l-10 9-10-9L12 0z"></path><path fill="#50B5E9" d="M4.6 14.7l7.4-3v9.6z"></path><path fill="#1F709A" d="M12 11.7l7.4 3-7.4 6.6z" opacity=".25"></path><path fill="#FFF" d="M12 11.7V3.6l7.4 11.1z" opacity=".25"></path><path fill="#FFF" d="M4.6 14.7L12 3.6v8.1z" opacity=".5"></path><path fill="#FFF" d="M7.2 14.3L12 7.2l4.8 7.1-4.8 4.3z" opacity=".5"></path></g></svg></div>'
+    case "exp":
+      return '<div style="width: 16px" data-v-062fc6e8="" class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path fill="#FFA623" d="M16 16l8-4-8-4-4-8-4 8-8 4 8 4 4 8z"></path><path fill="#FFF" d="M4.5 12l5-2.5L12 12zm7.5 7.5l-2.5-5L12 12zm7.5-7.5l-5 2.5L12 12zM12 4.5l2.5 5L12 12z" opacity=".25"></path><path fill="#BF7D1A" d="M19.5 12l-5-2.5L12 12z" opacity=".25"></path><path fill="#BF7D1A" d="M12 19.5l2.5-5L12 12z" opacity=".5"></path><path fill="#FFF" d="M4.5 12l5 2.5L12 12zM12 4.5l-2.5 5L12 12z" opacity=".5"></path><path fill="#FFF" d="M10.8 13.2L8.5 12l2.3-1.2L12 8.5l1.2 2.3 2.3 1.2-2.3 1.2-1.2 2.3z" opacity=".5"></path></g></svg></div>'
+    case "gp":
+      return '<div style="width: 16px" data-v-062fc6e8="" class="svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><circle cx="12" cy="12" r="12" fill="#FFA623"></circle><path fill="#FFF" d="M6.3 17.7c-3.1-3.1-3.1-8.2 0-11.3 3.1-3.1 8.2-3.1 11.3 0" opacity=".5"></path><path fill="#FFF" d="M17.7 6.3c3.1 3.1 3.1 8.2 0 11.3-3.1 3.1-8.2 3.1-11.3 0" opacity=".25"></path><path fill="#BF7D1A" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z" opacity=".5"></path><path fill="#BF7D1A" d="M13 9v2h-2V9H9v6h2v-2h2v2h2V9z" opacity=".75"></path></g></svg></div>'
+    default:
+      return '<div class="no valid stat selected"></div>'
+  }
+};
+
+const costColor = stat => {
+  switch (stat) {
+    case "hp":
+      return '#F74E52';
+    case "mp":
+      return '#2995cd'
+    case "exp":
+      return '#FFBE5D'
+    case "gp":
+      return '#fd7e14'
+    default:
+      return 'no color'
   }
 };
 
