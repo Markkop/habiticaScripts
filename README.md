6630909feee74e3d510269bfab0a9acc24f0e3fc
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2020-01-12 22:59:36 -0300

    version 2.1.0 (#14)
    * doc: ChangedStats
    
    * refac: script start
    
    * feat: spell reward info
    
    * feat: code formatting
    
    * feat: get tokens from settings/cookies
    
    * feat: set spell description
    
    * version bump to 2.1.0

--- README.md
+++ README.md
@@ -11,22 +11,22 @@ I've already used **Habitica** once and just started using it again to organize 
 That's how this project begins. By **consuming experience points** while using skills, I could slow down my leveling in exchange for other resources such hp, mana and gold. The challenging part here is to balance them correctly so these custom skills don't become too overpowered.  
 With this project I've been able to practice **Javascript, HTML, CSS, Testing and Functional Programming skills**.
 
-## How to Use
+## How to Use Habitica Custom Skills
 
-Install [Violentmonkey](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) / [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)  
-Click on its icon and `New user script`  
-Copy the content of [src/habiticaCustomSkills.user.js](src/habiticaCustomSkills) and paste on it  
-Add your `user` and `api` [tokens](https://habitica.com/user/settings/api) to the **tokens** object  
-Reload your [Habitica](https://habitica.com/)'s home
+1. Install [Violentmonkey](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) / [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+2. Click on its icon and `New user script`
+3. Copy the content of [src/habiticaCustomSkills.user.js](src/habiticaCustomSkills) and paste on it
+4. Reload your [Habitica](https://habitica.com/)'s home
 
 ## How to develop
 
 Check [this guide](https://violentmonkey.github.io/posts/how-to-edit-scripts-with-your-favorite-editor/) to "live delevop" this script.
 
-To change `changeStats()` and `checkRequirements()` functions it is advised to alter them in the [test file](src/stats.test.js) while running it with `npm test`.  
-If some wrong value is sent to Habitica's API, your stat will be zeroed and this can result in unexpected results, like death.  
-Don't forget to copy your changed functions to **habiticaCustomSkills.user.js** file
+-   To change `changeStats()` and `checkRequirements()` functions it is advised to alter them in the [test file](src/stats.test.js) while running it with `npm test`.
+-   If some wrong value is sent to Habitica's API, your stat will be zeroed and this can result in unexpected results, like death.
+-   Don't forget to copy your changed functions to **habiticaCustomSkills.user.js** file
 
+Tip: Use [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) and [Formatting Toggle](https://marketplace.visualstudio.com/items?itemName=tombonnike.vscode-status-bar-format-toggle) for code formatting
 
 # Progress History
 
@@ -38,18 +38,19 @@ Don't forget to copy your changed functions to **habiticaCustomSkills.user.js** 
 <hr>
 <img src="https://i.imgur.com/kIjk9qB.png"/>
 <hr>
+<img src="https://i.imgur.com/HMcUVEq.png"/>
+<hr>
 <img src="https://i.imgur.com/Ndh6dJ9.png"/>
 
 [(Printscreen of old testing file)](https://i.imgur.com/BLkLpcj.png)
 
-
 ## Some references
 
-- [JavaScript and Node Testing Best Practices](https://javascriptweekly.com/link/68555/14d64d4a39)
-- [Jest Fetch Mock](https://www.npmjs.com/package/jest-fetch-mock#installation-and-setup)
-- [How to mock specific module function in jest?](https://medium.com/@qjli/how-to-mock-specific-module-function-in-jest-715e39a391f4)
-- [Things I learned after writing tests for JS and HTML page](https://dev.to/snowleo208/things-i-learned-after-writing-tests-for-js-and-html-page-4lja)
-- [BFG Repo Cleaner](https://rtyley.github.io/bfg-repo-cleaner/)
-- [Reduce unit tests boilerplate with Jest’s .each syntax](https://itnext.io/reduce-unit-tests-boilerplate-with-jests-each-syntax-f5e48828437f)
-- [Jest ignore or exclude file/function/statement from test coverage](https://codewithhugo.com/jest-exclude-coverage/)
-- [How to read Test Coverage report generated using Jest](https://medium.com/@krishankantsinghal/how-to-read-test-coverage-report-generated-using-jest-c2d1cb70da8b)
+-   [JavaScript and Node Testing Best Practices](https://javascriptweekly.com/link/68555/14d64d4a39)
+-   [Jest Fetch Mock](https://www.npmjs.com/package/jest-fetch-mock#installation-and-setup)
+-   [How to mock specific module function in jest?](https://medium.com/@qjli/how-to-mock-specific-module-function-in-jest-715e39a391f4)
+-   [Things I learned after writing tests for JS and HTML page](https://dev.to/snowleo208/things-i-learned-after-writing-tests-for-js-and-html-page-4lja)
+-   [BFG Repo Cleaner](https://rtyley.github.io/bfg-repo-cleaner/)
+-   [Reduce unit tests boilerplate with Jest’s .each syntax](https://itnext.io/reduce-unit-tests-boilerplate-with-jests-each-syntax-f5e48828437f)
+-   [Jest ignore or exclude file/function/statement from test coverage](https://codewithhugo.com/jest-exclude-coverage/)
+-   [How to read Test Coverage report generated using Jest](https://medium.com/@krishankantsinghal/how-to-read-test-coverage-report-generated-using-jest-c2d1cb70da8b)
--- src/stats.test.js
+++ src/stats.test.js
@@ -7,7 +7,7 @@ const mockedStats = {
     gp: 164.177552698031,
     toNextLevel: 1640,
     maxHealth: 50,
-    maxMP: 390
+    maxMP: 390,
 }
 
 // Remember to copy/paste this function if changed on user.js file
@@ -16,8 +16,8 @@ const settings = {
         hp: 'maxHealth',
         mp: 'maxMP',
         exp: 'toNextLevel',
-        gp: 'gp'
-    }
+        gp: 'gp',
+    },
 }
 
 const changeStats = (modifiers, currentStats) => {
@@ -25,23 +25,21 @@ const changeStats = (modifiers, currentStats) => {
         const { resource, factor, type } = modifier
         const current = currentStats[resource]
         const integer = Number(String(current).split('.')[0])
-        const decimal = String(current).split('.')[1]
+        const decimal = String(current).split('.')[1] || ''
         const max = currentStats[settings.max[resource]]
 
         const newValue = {
             flat: integer + factor,
-            max: integer + (max/100 * factor),
-            current: integer + (current/100 * factor),
-            random: integer + Math.floor(Math.random() * (factor - 1)) + 1
+            max: integer + (max / 100) * factor,
+            current: integer + (current / 100) * factor,
+            random: integer + Math.floor(Math.random() * (factor - 1)) + 1,
         }
-
         const newStat = Math.round(newValue[type || 'flat'])
 
         return {
             ...newStats,
-            ['stats.'+resource]: Number(newStat + '.' + decimal)
+            [resource]: Number(newStat + '.' + decimal),
         }
-        
     }, {})
 }
 
@@ -49,184 +47,164 @@ describe('Stat', () => {
     test('is changed correctly with flat modifiers', () => {
         const spell = {
             modifiers: [
-                 { resource: 'hp', factor: -30, type: 'flat'},
-                 { resource: 'mp', factor: +10, type: 'flat'},
-                 { resource: 'exp', factor: 5, type: 'flat'},
-                 { resource: 'gp', factor: +5 }
-            ]
+                { resource: 'hp', factor: -30, type: 'flat' },
+                { resource: 'mp', factor: +10, type: 'flat' },
+                { resource: 'exp', factor: 5, type: 'flat' },
+                { resource: 'gp', factor: +5 },
+            ],
         }
 
         const expectedStats = {
-            'stats.hp': 7.03646565806994,
-            'stats.mp': 310.2300000000001,
-            'stats.exp': 1005.4094671046499,
-            'stats.gp': 169.177552698031
+            hp: 7.03646565806994,
+            mp: 310.2300000000001,
+            exp: 1005.4094671046499,
+            gp: 169.177552698031,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 
     test('is changed correctly with max modifiers', () => {
         const spell = {
             modifiers: [
-                 { resource: 'hp', factor: -20, type: 'max'},
-                 { resource: 'mp', factor: +10, type: 'max'},
-                 { resource: 'exp', factor: -50, type: 'max'},
-                 { resource: 'gp', factor: +10, type: 'max' }
-            ]
+                { resource: 'hp', factor: -20, type: 'max' },
+                { resource: 'mp', factor: +10, type: 'max' },
+                { resource: 'exp', factor: -50, type: 'max' },
+                { resource: 'gp', factor: +10, type: 'max' },
+            ],
         }
 
         const expectedStats = {
-            'stats.hp': 27.03646565806994,
-            'stats.mp': 339.2300000000001,
-            'stats.exp': 180.4094671046499,
-            'stats.gp': 180.177552698031
+            hp: 27.03646565806994,
+            mp: 339.2300000000001,
+            exp: 180.4094671046499,
+            gp: 180.177552698031,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 
     test('is changed correctly with current modifiers', () => {
         const spell = {
             modifiers: [
-                 { resource: 'hp', factor: -20, type: 'current'},
-                 { resource: 'mp', factor: +10, type: 'current'},
-                 { resource: 'exp', factor: -50, type: 'current'},
-                 { resource: 'gp', factor: +5, type: 'current' }
-            ]
+                { resource: 'hp', factor: -20, type: 'current' },
+                { resource: 'mp', factor: +10, type: 'current' },
+                { resource: 'exp', factor: -50, type: 'current' },
+                { resource: 'gp', factor: +5, type: 'current' },
+            ],
         }
 
         const expectedStats = {
-            'stats.hp': 30.03646565806994,
-            'stats.mp': 330.2300000000001,
-            'stats.exp': 500.4094671046499,
-            'stats.gp': 172.177552698031
+            hp: 30.03646565806994,
+            mp: 330.2300000000001,
+            exp: 500.4094671046499,
+            gp: 172.177552698031,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 
     test('is changed correctly with random modifiers', () => {
-        const mockMath = Object.create(global.Math);
-        mockMath.random = () => 0.5;
-        global.Math = mockMath;
+        const mockMath = Object.create(global.Math)
+        mockMath.random = () => 0.5
+        global.Math = mockMath
 
         const spell = {
             modifiers: [
-                 { resource: 'hp', factor: -10, type: 'random'},
-                 { resource: 'mp', factor: +10, type: 'random'},
-                 { resource: 'exp', factor: -100, type: 'random'},
-                 { resource: 'gp', factor: +50, type: 'random' }
-            ]
+                { resource: 'hp', factor: -10, type: 'random' },
+                { resource: 'mp', factor: +10, type: 'random' },
+                { resource: 'exp', factor: -100, type: 'random' },
+                { resource: 'gp', factor: +50, type: 'random' },
+            ],
         }
 
         const expectedStats = {
-            'stats.hp': 32.03646565806994,
-            'stats.mp': 305.2300000000001,
-            'stats.exp': 950.4094671046499,
-            'stats.gp': 189.177552698031
+            hp: 32.03646565806994,
+            mp: 305.2300000000001,
+            exp: 950.4094671046499,
+            gp: 189.177552698031,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 
     test('remains equal if modifier value is zero', () => {
         const spell = {
             modifiers: [
-                 { resource: 'hp', factor: 0, type: 'flat'},
-                 { resource: 'mp', factor: 0, type: 'max'},
-                 { resource: 'exp', factor: 0, type: 'current'},
-                 { resource: 'gp', factor: 0, type: 'random' }
-            ]
+                { resource: 'hp', factor: 0, type: 'flat' },
+                { resource: 'mp', factor: 0, type: 'max' },
+                { resource: 'exp', factor: 0, type: 'current' },
+                { resource: 'gp', factor: 0, type: 'random' },
+            ],
         }
 
         const expectedStats = {
-            'stats.hp': 37.03646565806994,
-            'stats.mp': 300.2300000000001,
-            'stats.exp': 1000.4094671046499,
-            'stats.gp': 164.177552698031,
+            hp: 37.03646565806994,
+            mp: 300.2300000000001,
+            exp: 1000.4094671046499,
+            gp: 164.177552698031,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 
     test('is changed when only one modifier is provided', () => {
         const spell = {
-            modifiers: [
-                 {resource: 'hp', factor: +10, type: 'flat'},
-            ]
+            modifiers: [{ resource: 'hp', factor: +10, type: 'flat' }],
         }
 
         const expectedStats = {
-            'stats.hp': 47.03646565806994,
+            hp: 47.03646565806994,
         }
 
         const newStats = changeStats(spell.modifiers, mockedStats)
-
         expect(newStats).toEqual(expectedStats)
     })
 })
 
 describe('Checkrequirements', () => {
-    window.alert = () => {}
+    const logs = () => {}
 
     const checkRequirements = (newStats, currentStats) => {
-        const newStatsPairs = Object.entries(newStats);
-      
+        const newStatsPairs = Object.entries(newStats)
         return newStatsPairs.reduce((result, [key, value]) => {
-            const stat = key.split('.')[1]
-            const requiredValue = currentStats[stat] + value * -1
-            const message = `You need ${requiredValue} ${stat} to cast this skill`
-    
+            const requiredValue = currentStats[key] + value * -1
+
             if (value < 0) {
-                console.log(message)
-                alert(message)
+                logs(`You need ${requiredValue} ${key} to cast this skill`, { newStats }, { currentStats })
                 return false
             }
-    
+
             return result
-    
         }, true)
-    };
+    }
 
     test("returns false if can't cast a spell", () => {
-
         const newStats = {
-            'stats.hp': 37,
-            'stats.mp': -1,
-            'stats.exp': 1000,
-            'stats.gp': 164,
+            hp: 37,
+            mp: -1,
+            exp: 1000,
+            gp: 164,
         }
 
-
-        const canCastSpell = checkRequirements(newStats ,mockedStats)
-
+        const canCastSpell = checkRequirements(newStats, mockedStats)
         expect(canCastSpell).toBeFalsy()
     })
 
-    test("returns true if can cast a spell", () => {
-        window.alert = msg => console.log(msg)
-
+    test('returns true if can cast a spell', () => {
         const newStats = {
-            'stats.hp': 37,
-            'stats.mp': 100,
-            'stats.exp': 1000,
-            'stats.gp': 164,
+            hp: 37,
+            mp: 100,
+            exp: 1000,
+            gp: 164,
         }
 
-
-        const canCastSpell = checkRequirements(newStats ,mockedStats)
-
+        const canCastSpell = checkRequirements(newStats, mockedStats)
         expect(canCastSpell).toBeTruthy()
     })
 })
