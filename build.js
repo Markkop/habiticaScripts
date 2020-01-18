c1abc8e98eeb51349bb8a2c05a89efaaef452f94
Mark Kop <mk.marcelo.mk@gmail.com>
2020-01-18 18:49:36 -0300

    feat: auto build + tests watcher

--- build.js
+++ build.js
@@ -2,13 +2,18 @@ const { execSync } = require('child_process')
 const inquirer = require('inquirer')
 
 /**
- * Executes the rollup command to the given script
+ * Executes the an action to the given script
  *
  * @param { "habiticaCustomSkills" | "habiticaShortcuts" } script
+ * @param { "dev" | "build" } action
  * @returns { "habiticaCustomSkills" | "habiticaShortcuts" } script
  */
-function deployScript (script) {
-    execSync(`rollup --config src/${script}/rollup.config.js`, { stdio: 'inherit' })
+function deployScript (script, action) {
+    if(action === 'dev') {
+      execSync(`rollup --config src/${script}/rollup.config.js --watch & jest src/${script} --watch`, { stdio: 'inherit' })
+    } else {
+      execSync(`rollup --config src/${script}/rollup.config.js`, { stdio: 'inherit' })
+    }
   return script
 }
 
@@ -22,6 +27,15 @@ inquirer.prompt([
       { name: 'habiticaShortcuts' },
       { name: 'habiticaNotifications', disabled: 'WIP' }
     ]
+  },
+  {
+    type: 'list',
+    name: 'action',
+    message: "Select which action you want to perform\nPS: for dev action you'll need to exit this terminal when done. Know how to fix? Contribute :D",
+    choices: [
+      { name: 'dev' },
+      { name: 'build' },
+    ]
   }
 ])
-  .then(({ script }) => deployScript(script))
+  .then(({ script, action }) => deployScript(script, action))
